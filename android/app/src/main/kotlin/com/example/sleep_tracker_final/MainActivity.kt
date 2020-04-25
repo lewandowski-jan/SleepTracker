9a39a00e106dff263c3ed0144721016566bc4b02
Jan Lewandowski <iasiu@icloud.com>
2020-04-26 00:24:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sleep_tracker_final/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sleep_tracker_final
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
