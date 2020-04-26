a8ba9b5e58a7e9bf3fbd2aa1779ddd08e8f7a582
Jan Lewandowski <iasiu@icloud.com>
2020-04-26 14:34:06 +0200

    added secondpage: numberpicker, dropdown list, saving data form secondpage to sleepRecords list

--- lib/pages/secondpage.dart
+++ lib/pages/secondpage.dart
@@ -20,7 +20,50 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
-  _SecondPageState();
+  var _options = ['-', 'Night\'s Sleep', 'Nap'];
+  var _currentItemSelected = '-';
+
+  int _currentHourValue = 0;
+  int _currentMinuteValue = 0;
+  NumberPicker decimalNumberPicker;
+
+  Future _showHourDialog() async {
+    await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return new NumberPickerDialog.integer(
+          minValue: 0,
+          maxValue: 12,
+          step: 1,
+          initialIntegerValue: _currentHourValue,
+          title: Text("Pick number of hours"),
+        );
+      },
+    ).then((num value) {
+      if (value != null) {
+        setState(() => _currentHourValue = value);
+      }
+    });
+  }
+
+  Future _showMinuteDialog() async {
+    await showDialog<int>(
+      context: context,
+      builder: (BuildContext context) {
+        return new NumberPickerDialog.integer(
+          minValue: 0,
+          maxValue: 59,
+          step: 1,
+          initialIntegerValue: _currentMinuteValue,
+          title: Text("Pick number of minutes"),
+        );
+      },
+    ).then((num value) {
+      if (value != null) {
+        setState(() => _currentMinuteValue = value);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,7 +127,7 @@ class _SecondPageState extends State<SecondPage> {
               ),
             ),
             Container(
-                decoration: BoxDecoration(
+              decoration: BoxDecoration(
                 border: Border(
                   bottom: BorderSide(color: Colors.black54),
                 ),
@@ -106,16 +149,26 @@ class _SecondPageState extends State<SecondPage> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
+                      DropdownButton<String>(
+                        hint: Text('Night, nap, etc.'),
+                        isDense: true,
+                        items: _options.map(
+                          (String dropDownStringItem) {
+                            return DropdownMenuItem<String>(
+                              value: dropDownStringItem,
+                              child: Text(dropDownStringItem),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (String value) {
+                          setState(() {
+                            this._currentItemSelected = value;
+                          });
+                        },
+                        value: _currentItemSelected,
+                      ),
                       SizedBox(
                         height: 2.0,
-                      ),
-                      Text(
-                        '-',
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16.0,
-                        ),
                       ),
                     ],
                   ),
@@ -123,18 +176,15 @@ class _SecondPageState extends State<SecondPage> {
               ),
             ),
             Container(
-                decoration: BoxDecoration(
+              decoration: BoxDecoration(
                 border: Border(
                   bottom: BorderSide(color: Colors.black54),
                 ),
               ),
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20, top: 5.0, bottom: 5.0),
               child: Row(
                 children: <Widget>[
-                  Icon(
-                    Icons.timer,
-                    color: Colors.indigo,
-                    size: 30.0),
+                  Icon(Icons.timer, color: Colors.indigo, size: 30.0),
                   SizedBox(
                     width: 14.0,
                   ),
@@ -151,15 +201,68 @@ class _SecondPageState extends State<SecondPage> {
                       SizedBox(
                         height: 2.0,
                       ),
-                      Text(
-                        '-',
-                        style: TextStyle(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16.0,
+                      Row(children: [
+                        FlatButton(
+                          color: Colors.black12,
+                          onPressed: () => _showHourDialog(),
+                          child: Text(
+                            '$_currentHourValue hour',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ),
-                      ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        FlatButton(
+                          color: Colors.black26,
+                          onPressed: () => _showMinuteDialog(),
+                          child: Text(
+                            '$_currentMinuteValue minute',
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      ])
                     ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 40.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MaterialButton(
+                      padding: EdgeInsets.all(20.0),
+                      color: Colors.indigo,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () {
+                        if (this._currentItemSelected != '-' && (this._currentHourValue != 0 || this._currentMinuteValue != 0)) {
+                          var time = '${widget.now.hour.toString().padLeft(2, '0')}:${widget.now.minute.toString().padLeft(2, '0')}';
+                          var type = this._currentItemSelected;
+                          var duration = '$_currentHourValue hour $_currentMinuteValue minute';
+
+                          widget.sleepRecords.insert(0, SleepRecord(time, type, duration));
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Save',
+                        style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ],
               ),
