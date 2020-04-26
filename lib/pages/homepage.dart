733d2d1a8b3339fe007c731626f758d0e7cfd91a
Jan Lewandowski <iasiu@icloud.com>
2020-04-26 12:33:03 +0200

    added getListTile()

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -27,104 +27,150 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(top: 25.0),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.all(
-                const Radius.circular(30.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 25.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.all(
+                  const Radius.circular(30.0),
+                ),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child: Icon(
+                Icons.brightness_2,
+                color: Colors.white,
+                size: 25.0,
               ),
             ),
-            padding: const EdgeInsets.all(10.0),
-            child: Icon(
-              Icons.brightness_2,
-              color: Colors.white,
-              size: 25.0,
+            Container(
+              margin: const EdgeInsets.only(left: 35.0, top: 25.0, right: 35.0),
+              child: Text(
+                'Get to know your baby\'s sleep patterns and keep track of how much sleep they are getting here.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black54),
+              ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 35.0, top: 25.0, right: 35.0),
-            child: Text(
-              'Get to know your baby\'s sleep patterns and keep track of how much sleep they are getting here.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.w600,
-                color: Colors.black54
-              )
-            )
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10.0, top: 25.0, right: 10.0, bottom: 70.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: MaterialButton(
-                    padding: const EdgeInsets.all(20.0),
-                    color: Colors.indigo,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0)
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      'Add new sleeping record',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+            Container(
+              margin: const EdgeInsets.only(
+                  left: 10.0, top: 25.0, right: 10.0, bottom: 70.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MaterialButton(
+                      padding: const EdgeInsets.all(20.0),
+                      color: Colors.indigo,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0)),
+                      onPressed: () {},
+                      child: Text(
+                        'Add new sleeping record',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  )
-                )
-              ],
-            )
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(left: 15.0),
-                child: Text(
-                  '${getDay(now.weekday)}, ${now.day} ${getMonth(now.month)} ${now.year}',
-                  style: TextStyle(
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87
-                  )
-                )
-              )
-            ],
-          ),
-          Container(
-            child: Expanded(
-              child: Container(
-                margin: const EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(color: Colors.grey, blurRadius: 20.0),
-                  ],
-                  borderRadius: BorderRadius.only(
-                    topLeft: const Radius.circular(10.0),
-                    topRight: const Radius.circular(10.0),
                   ),
-                ),
-                child: ListView.separated(
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text('$index')
-                    );
-                  },
-                  separatorBuilder: (context, index) => Divider(
-                    color: Colors.black87,
-                  ),
-                )
+                ],
               ),
             ),
-          ),
-        ]),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(left: 15.0),
+                  child: Text(
+                    '${getDay(now.weekday)}, ${now.day} ${getMonth(now.month)} ${now.year}',
+                    style: TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black87),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              child: Expanded(
+                child: Container(
+                  margin:
+                      const EdgeInsets.only(top: 25.0, left: 20.0, right: 20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(color: Colors.grey, blurRadius: 20.0),
+                    ],
+                    borderRadius: BorderRadius.only(
+                      topLeft: const Radius.circular(10.0),
+                      topRight: const Radius.circular(10.0),
+                    ),
+                  ),
+                  child: ListView.separated(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return getListTile("11:42", "Nap", "1 hour 3 minute");
+                    },
+                    separatorBuilder: (context, index) => Divider(
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget getListTile(time, type, duration) {
+  var listTile = ListTile(
+    title: Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 4.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  '$time',
+                  style: TextStyle(fontWeight: FontWeight.w800),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '$type',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: Colors.indigo,
+                  ),
+                ),
+                Text(
+                  '$duration',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black54,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+
+  return listTile;
 }
